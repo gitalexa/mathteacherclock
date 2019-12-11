7bc9e3c8156b08be66af0a8453fb5f80a7390f6a
Alexander Probst <alex@probst.tv>
2019-12-11 13:58:18 +0100

    Hinzufügen weiterer Testscripts ini Datei zum Einlesen der Parameter

--- ReadFile.py
+++ ReadFile.py
@@ -1,4 +1,5 @@
 import urllib2
+from xml.dom import minidom
 
 
 def main():
@@ -7,6 +8,30 @@ def main():
 
     for line in data:
         print line
+
+    # parse an xml file by name
+    mydoc = minidom.parse('clockini.xml')
+
+    items = mydoc.getElementsByTagName('item')
+
+    # one specific item attribute
+    print('Item #2 attribute:')
+    print(items[1].attributes['name'].value)
+
+    # all item attributes
+    print('\nAll attributes:')
+    for elem in items:
+        print(elem.attributes['name'].value)
+
+    # one specific item's data
+    print('\nItem #2 data:')
+    print(items[1].firstChild.data)
+    print(items[1].childNodes[0].data)
+
+    # all items data
+    print('\nAll item data:')
+    for elem in items:
+        print(elem.firstChild.data)
 
 if __name__ == "__main__":
         main()

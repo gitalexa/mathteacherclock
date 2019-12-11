540d1905ad02d7c3bb631fc390bf0b1a569fd48c
Alexander Probst <alex@probst.tv>
2019-12-11 13:33:33 +0100

    Hinzufügen weiterer Testscripts ini Datei zum Einlesen der Parameter

--- ReadFile.py
+++ ReadFile.py
@@ -2,11 +2,7 @@ import urllib2
 
 
 def main():
-
-
-# Setup for Banggood version of 4 x 8x8 LED Matrix (https://bit.ly/2Gywazb)
-
-    data = urllib2.urlopen("http://www.google.com").read(20000) # read only 20 000 chars
+    data = urllib2.urlopen("https://raw.githubusercontent.com/gitalexa/mathteacherclock/master/clock.ini").read(20000) # read only 20 000 chars
     data = data.split("\n") # then split it into lines
 
     for line in data:

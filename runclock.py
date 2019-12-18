194fe6cde3de02013c908abc218df9d839e0048a
Alexander Probst <alex@probst.tv>
2019-12-18 10:30:41 +0100

    creating runclock for testing Clock.py

---
+++ runclock.py
@@ -0,0 +1,26 @@
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT
+from Clock import main as clock
+
+
+def main():
+    # Setup for Banggood version of 4 x 8x8 LED Matrix (https://bit.ly/2Gywazb)
+    serial = spi(port=0, device=0, gpio=noop())
+    # device = max7219(serial, cascaded=8, block_orientation=-90, blocks_arranged_in_reverse_order=False)
+    device = max7219(serial, width=32, height=16, block_orientation=-90, rotate=0)
+
+    # device.contrast(16)
+    while True:
+        c = clock()
+        c.update_class(2)
+        with canvas(device) as draw:
+            text(draw, (0, current_y), "123", fill="white", font=proportional(TINY_FONT))
+            text(draw, (15, current_y), ":", fill="white", font=proportional(TINY_FONT))
+            text(draw, (17, current_y), "333", fill="white", font=proportional(TINY_FONT))
+
+
+if __name__ == "__main__":
+        main()
--- testclock.py
+++ testclock.py
@@ -1,48 +1,16 @@
 #!/usr/bin/env python
-import time
-from datetime import datetime
+
 
 #from luma.led_matrix.device import max7219
 #from luma.core.interface.serial import spi, noop
 #from luma.core.render import canvas
 #from luma.core.legacy import text, show_message
 #from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT
-from random import randrange
 
-from TestClass import MyClass, Person
-
-
-def minute_change(device):
-    '''When we reach a minute change, animate it.'''
-    hours = datetime.now().strftime('%H')
-    minutes = datetime.now().strftime('%M')
-
-    def helper(current_y):
-        #with canvas(device) as draw:
-            #text(draw, (0, 1), hours, fill="white", font=proportional(TINY_FONT))
-            #text(draw, (15, 1), ":", fill="white", font=proportional(TINY_FONT))
-            #text(draw, (17, current_y), minutes, fill="white", font=proportional(TINY_FONT))
-        time.sleep(0.1)
-
-    #for current_y in range(1, 9):
-        #helper(current_y)
-    minutes = datetime.now().strftime('%M')
-    #for current_y in range(9, 1, -1):
-        #helper(current_y)
+from Clock import main as clock
 
 
-def animation(device, from_y, to_y):
-    '''Animate the whole thing, moving it into/out of the abyss.'''
-    hourstime = datetime.now().strftime('%H')
-    mintime = datetime.now().strftime('%M')
-    current_y = from_y
-    #while current_y != to_y:
-        #with canvas(device) as draw:
-            #text(draw, (0, current_y), hourstime, fill="white", font=proportional(TINY_FONT))
-            #text(draw, (15, current_y), ":", fill="white", font=proportional(TINY_FONT))
-            #text(draw, (17, current_y), mintime, fill="white", font=proportional(TINY_FONT))
-    time.sleep(0.1)
-        #current_y += 1 if to_y > from_y else -1
+
 
 
 def main():
@@ -56,47 +24,16 @@ def main():
     # The time ascends from the abyss...
     #animation(device, 8, 1)
 
-    toggle = False  # Toggle the second indicator every second
-    while True:
-        toggle = not toggle
-        sec = datetime.now().second
-        if sec == 59:
-            # When we change minutes, animate the minute change
-            #minute_change(device)
-            print("sss")
-        elif sec == 30:
-            # Half-way through each minute, display the complete date/time,
-            # animating the time display into and out of the abyss.
-            full_msg = time.ctime()
-            #animation(device, 1, 8)
-            #show_message(device, full_msg, fill="white", font=proportional(TINY_FONT))
-            #animation(device, 8, 1)
-            print("30")
-        else:
-            # Do the following twice a second (so the seconds' indicator blips).
-            # I'd optimize if I had to - but what's the point?
-            # Even my Raspberry PI2 can do this at 4% of a single one of the 4 cores!
-            hours = datetime.now().strftime('%H')
-            minutes = datetime.now().strftime('%M')
-            #with canvas(device) as draw:
-                #text(draw, (5, 1), hours, fill="white", font=proportional(TINY_FONT))
-                #text(draw, (15, 1), ":" if toggle else " ", fill="white", font=proportional(TINY_FONT))
-                #text(draw, (17, 1), minutes, fill="white", font=proportional(TINY_FONT))
+    if __name__ == '__main__':
+        root = main()
 
-                #text(draw, (17, 9), minutes, fill="white", font=proportional(TINY_FONT))
-
-            p1 = MyClass()
-            print(p1.x)
-
-            p1 = Person("John", 36)
-
-            print(p1.name)
-            print(p1.age)
-
-            print(randrange(10))
-            rmin = randrange(1, 59)
-                #text(draw, (1, 9), str(rmin), fill="white", font=proportional(TINY_FONT))
-            time.sleep(0.5)
+        # Creating Main Loop
+        while True:
+            root.update_class(2)
+            # with canvas(device) as draw:
+            # text(draw, (0, current_y), hourstime, fill="white", font=proportional(TINY_FONT))
+            # text(draw, (15, current_y), ":", fill="white", font=proportional(TINY_FONT))
+            # text(draw, (17, current_y), mintime, fill="white", font=proportional(TINY_FONT))
 
 
 if __name__ == "__main__":

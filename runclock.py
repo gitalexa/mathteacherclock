3e5c5b52bce6e73c831dddd3999b73b48800d37d
Alexander Probst <alex@probst.tv>
2019-12-18 10:46:38 +0100

    chnanging runclock for testing Clock.py

--- runclock.py
+++ runclock.py
@@ -1,3 +1,4 @@
+
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -9,7 +10,7 @@ from Clock import main as clock
 def main():
     # Setup for Banggood version of 4 x 8x8 LED Matrix (https://bit.ly/2Gywazb)
     serial = spi(port=0, device=0, gpio=noop())
-    # device = max7219(serial, cascaded=8, block_orientation=-90, blocks_arranged_in_reverse_order=False)
+    # device = max7219(serial, cascaded=8, block_orientation=-90, blocks_arranged_in_reverse_ord$
     device = max7219(serial, width=32, height=16, block_orientation=-90, rotate=0)
 
     # device.contrast(16)
@@ -17,9 +18,8 @@ def main():
         c = clock()
         c.update_class(2)
         with canvas(device) as draw:
-            text(draw, (0, current_y), "123", fill="white", font=proportional(TINY_FONT))
-            text(draw, (15, current_y), ":", fill="white", font=proportional(TINY_FONT))
-            text(draw, (17, current_y), "333", fill="white", font=proportional(TINY_FONT))
+            text(draw, (2, 1), "123", fill="white", font=proportional(TINY_FONT))
+            text(draw, (4, 9), "333", fill="white", font=proportional(TINY_FONT))
 
 
 if __name__ == "__main__":
